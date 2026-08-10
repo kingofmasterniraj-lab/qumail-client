79072f0deedf86cde5b70a4237e2d3ba09cb949f
kingofmasterniraj-lab <kingofmasterniraj@gmail.com>
2026-08-10 19:40:16 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
+# src/main.py
+import sys
+from PyQt6.QtWidgets import QApplication
+from src.gui.main_window import QuMailWindow
+
+def main():
+    app = QApplication(sys.argv)
+    window = QuMailWindow()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
 
